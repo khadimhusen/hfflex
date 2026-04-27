33d9d44ee7d5a8e156a549a1e7209ebac82b6cd5
khadimhusen <khadimhusen@gmail.com>
2026-04-27 15:04:00 +0530

    banklist filter added based on bill date

--- bank/filters.py
+++ bank/filters.py
@@ -4,12 +4,16 @@ from .models import Cheque
 
 class ChequeFilter(django_filters.FilterSet):
 
-    cheque__from = django_filters.DateFilter(label="From", field_name='cheque_date', lookup_expr='gte',
+    cheque_from = django_filters.DateFilter(label="From", field_name='cheque_date', lookup_expr='gte',
                                             input_formats=('%d/%m/%Y',))
-    cheque__to = django_filters.DateFilter(label="To", field_name='cheque_date', lookup_expr='lte',
+    cheque_to = django_filters.DateFilter(label="To", field_name='cheque_date', lookup_expr='lte',
                                             input_formats=('%d/%m/%Y',))
     party=django_filters.CharFilter(label='Party Name',field_name="party",lookup_expr="icontains")
+    from_bill = django_filters.DateFilter(label="From Bill",field_name='bill_date', lookup_expr='gte',input_formats=('%d/%m/%Y',))
+    to_bill = django_filters.DateFilter(label="To Bill",field_name='bill_date', lookup_expr='lte',input_formats=('%d/%m/%Y',))
+
+
     class Meta:
         model = Cheque
-        fields = ["bank","number","party","status","bill_number","lock_record","edited","editedby"]
+        fields = ["bank","number","party","status","bill_number","lock_record","editedby"]
 
