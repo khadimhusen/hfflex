6a456fc5e74867862925921ed4a8ec996c9f856c
khadimhusen <khadimhusen@gmail.com>
2026-08-28 22:05:05 +0530

    Rebuild CRM static assets with itemmaster list error notification

---
+++ crm_static/assets/new-Cy1dYyh8.js
@@ -0,0 +1,1 @@
+import{J as e,pt as t}from"./use-router-link-CLide7n7.js";import{t as n}from"./QuotationForm-DPOxasCg.js";var r={__name:`new`,setup(r){return(r,i)=>(t(),e(n))}};export{r as default};
