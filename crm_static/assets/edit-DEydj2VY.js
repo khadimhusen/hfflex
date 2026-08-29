5860d6c91955a8e9730ff28d83cfcda12633694c
khadimhusen <khadimhusen@gmail.com>
2026-08-29 20:43:59 +0530

    deleted crm_folder

--- crm_static/assets/edit-DEydj2VY.js
+++
@@ -1,1 +0,0 @@
-import{At as e,J as t,mt as n}from"./use-router-link-Rhu-fyfW.js";import{b as r}from"./index-DI2Q1mhi.js";import{t as i}from"./QuotationForm-bQggI_kf.js";var a={__name:`edit`,setup(a){let o=r();return(r,a)=>(n(),t(i,{"quotation-id":e(o).params.id},null,8,[`quotation-id`]))}};export{a as default};
--- crm_static/assets/gst-CyKqFZEU.js
+++
@@ -1,1 +0,0 @@
-var e=/^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z][1-9A-Z]Z[0-9A-Z]$/;function t(t){return!t||e.test(t.trim().toUpperCase())||`Enter a valid 15-character GST number`}export{t};
