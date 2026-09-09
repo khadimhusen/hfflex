41e3db075b34b2dcb1422a23d5ce5f5edf7a7944
khadimhusen <khadimhusen@gmail.com>
2026-09-09 09:43:44 +0530

    costing sheet correctoin done

---
+++ crm_static/assets/edit-cdo91CEH.js
@@ -0,0 +1,1 @@
+import{U as e,dt as t,jt as n}from"./use-router-link-C2rrFOSi.js";import{k as r}from"./index-JW9w6cmz.js";import{t as i}from"./QuotationForm-DOnFmm2H.js";var a={__name:`edit`,setup(a){let o=r();return(r,a)=>(t(),e(i,{"quotation-id":n(o).params.id},null,8,[`quotation-id`]))}};export{a as default};
